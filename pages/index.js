9f314d01cb60d1fa9445765fa197af1dfcbd6fb5
mc6ril <cyril.lesot@yahoo.fr>
2021-09-10 17:17:19 +0200

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
-/* eslint-disable react/no-unescaped-entities */
 import Layout from "../components/Layout";
 import HardSkills from "../components/HardSkills";
 import Presentation from "../components/Presentation";
 import Profil from "../components/Profil";
 
+import { useEffect, useState } from "react";
+import json from "../assets/data/projects.json";
+
 export default function Home() {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    setProjects(json);
+  }, []);
+  console.log(projects);
   return (
     <Layout page={"Portfolio Cyril Lesot - home"}>
       <section className="home">
