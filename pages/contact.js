c3a12c60b9a33bfd8545a6bcfed98493a273f561
mc6ril <cyril.lesot@yahoo.fr>
2021-09-20 12:53:44 +0200

    ajout de la fonctionnalité de la page contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,35 @@
 import Header from "../components/Header";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Contact() {
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+  console.log(message);
+
+  const onHandleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `https://portfolio-cyril-lesot.herokuapp.com/form`,
+        {
+          name: name,
+          email: email,
+          description: description,
+        }
+      );
+      if (response.status === 200) {
+        setMessage("Email envoyé avec succés");
+      }
+    } catch (error) {
+      setMessage("Une erreur est survenue. Veuillez réessayer.");
+    }
+  };
+
   return (
     <section className="contact layout">
       <Header />
@@ -17,22 +45,39 @@ export default function Contact() {
         />
       </div>
 
-      <form className="contact-form">
+      <form className="contact-form" onSubmit={onHandleSubmit}>
         <div className="first-inputs">
           <div className="control">
             <label htmlFor="name">Nom</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              onChange={(e) => setName(e.currentTarget.value)}
+            />
           </div>
           <div className="control">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
           </div>
         </div>
         <div>
           <div className="control">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="5" required></textarea>
+            <textarea
+              name="message"
+              id="message"
+              rows="5"
+              required
+              onChange={(e) => setDescription(e.currentTarget.value)}
+            ></textarea>
           </div>
+          {message && <p>{message}</p>}
         </div>
 
         <input type="submit" value="Envoyer" />
