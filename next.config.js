eea0a612a853d9609816c57e80ca1e0fab43db5e
Cyril Lesot <cyrillesot@perifit.co>
2026-03-31 21:12:36 +0200

    fix vercel loop

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        // Support pour React 19
-        reactCompiler: false, // Désactivé pour l'instant car expérimental
-    },
+    reactCompiler: false,
     // Configuration pour React 19
     compiler: {
         // Désactiver le minification si nécessaire pour le debug
         removeConsole: process.env.NODE_ENV === "production",
+    },
+    images: {
+        qualities: [75, 100],
     },
 };
 
